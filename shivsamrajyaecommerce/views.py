c5d0c8eaa6b1794865ea1fccec0a523e1757a1ab
= <=>
2025-03-08 20:06:15 +0530

    mighty push

--- shivsamrajyaecommerce/views.py
+++ shivsamrajyaecommerce/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render # type: ignore
 
 def base(request):
     return render(request,'base.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'register.html')
