b378dcbe01233468cd21608af6ec1131b376377f
= <=ashwinisshinde279@gmail.com>
2025-03-15 10:40:29 +0530

    push

--- shivsamrajyaecommerce/settings.py
+++ shivsamrajyaecommerce/settings.py
@@ -37,14 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'taluka',
     'village'
-=======
+
     'state',
     'district'
     
->>>>>>> 1df3834b3ba7373bb4f91ae42c112e73ca20d8b4
 ]
 
 MIDDLEWARE = [
