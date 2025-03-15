cd6b527d98d677f7a1a33e9ff376674fb3fdce28
= <=>
2025-03-15 18:41:19 +0530

    push

--- village/admin.py
+++ village/admin.py
@@ -1,9 +1,5 @@
 from django.contrib import admin # type: ignore
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5b63a5203b2de69b79bfc3e2f45705f0a1f665dc
 from .models import Village
 
 class VillageAdmin(admin.ModelAdmin):
