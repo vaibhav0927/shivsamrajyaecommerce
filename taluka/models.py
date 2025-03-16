4331dc05f4c0b788bbc509070769a0473a0c8f2c
= <=>
2025-03-15 18:26:02 -0700

    sd

--- taluka/models.py
+++ taluka/models.py
@@ -1,12 +1,12 @@
 from django.db import models # type: ignore
 
-from  district.models import district
+from  district.models import District
 from autoslug import AutoSlugField # type: ignore
 
 # Create your models here.
 class Taluka(models.Model): 
     taluka_id = models.AutoField(primary_key=True)
-    district=models.ForeignKey(district,on_delete=models.CASCADE,null=True)
+    district=models.ForeignKey(District,on_delete=models.CASCADE,null=True)
     taluka_name = models.CharField(max_length=255)
     taluka_slug = AutoSlugField(populate_from="taluka_name", unique=True)
     class Meta:
