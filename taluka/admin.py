31ebace7fa4aeb566c31dea68110e10fe6115a83
= <=>
2025-03-15 18:17:28 -0700

    sa

--- taluka/admin.py
+++ taluka/admin.py
@@ -1,14 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin # type: ignore
+
 from .models import Taluka
 
-
-# Register your models here.
 class TalukaAdmin(admin.ModelAdmin):
-    list_display = ('taluka_id','taluka_name')
+    list_display=('taluka_id','taluka_name','taluka_slug','district_name')
     def district_name(self,obj):
-        return obj.taluka.taluka_name
-    def district_slug(self,obj):
-        return obj.district.district_slug  
-    district_name.short_description = "District Name"     
-admin.site.register(Taluka,TalukaAdmin)
+        return obj.district.district_name 
+
+
+admin.site.register(Taluka, TalukaAdmin)
 
--- village/models.py
+++ village/models.py
@@ -1,12 +1,15 @@
-from django.db import models
-from taluka.models import Taluka
+from django.db import models # type: ignore
+
+from  taluka.models import taluka
+from autoslug import AutoSlugField # type: ignore
 
 # Create your models here.
-class Village(models.Model):
+class Village(models.Model): 
     village_id = models.AutoField(primary_key=True)
+    taluka=models.ForeignKey(taluka,on_delete=models.CASCADE,null=True)
     village_name = models.CharField(max_length=255)
-    taluka = models.ForeignKey(Taluka,on_delete=models.CASCADE)
+    village_slug = AutoSlugField(populate_from="village_name", unique=True)
     class Meta:
         db_table = "tbl_village"
-
-
+    def _str_(self):
+        return self.village_name
