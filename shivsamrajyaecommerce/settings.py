6f647f06ab5c138c33335d4fada821e895ec84f2
= <=ashwinisshinde279@gmail.com>
2025-03-26 16:20:19 +0530

    push

--- shivsamrajyaecommerce/settings.py
+++ shivsamrajyaecommerce/settings.py
@@ -53,15 +53,8 @@ INSTALLED_APPS = [
     'taxtype',
     'alternateunit',
     'contactus',
-<<<<<<< HEAD
     'wishlist',
     'cart',
-=======
-
-    'wishlist',
-    'cart',
-
->>>>>>> b0bda06b6430c4da190883c62393e73ed9eaafc0
 
 ]
 
